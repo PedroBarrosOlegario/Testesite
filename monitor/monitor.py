59afc0f6928b2645c33dd30a5cea15c0215f91bc
Pedro Henrique de Barros Olegario <pedro.olegario@sga.pucminas.br>
2025-11-25 09:32:49 -0300

    Update URL for process monitoring

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -5,7 +5,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-URL = "https://pje-consulta-publica.tjmg.jus.br/pje/ConsultaPublica/DetalheProcessoConsultaPublica/listView.seam?ca=ba079116e770156ed0f39cfe7972e4d3018e461e9fa107a5"
+URL = "https://pje-consulta-publica.tjmg.jus.br/pje/ConsultaPublica/DetalheProcessoConsultaPublica/listView.seam?ca=ba079116e770156ec2292f7b681e8bce018e461e9fa107a5"
 ULTIMO_ARQUIVO = os.path.join(os.path.dirname(__file__), "ultima.txt")
 
 def pegar_movimentacoes():
@@ -52,4 +52,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
